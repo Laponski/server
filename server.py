5bb90882e40a15b2bc8f5efe22d4ba31215d5d0d
Laponski <lapotap2007@gmail.com>
2024-06-26 16:41:35 +0200

    feat: sharter version: if path is lapo, it shows tapinassi
    BREAKING CHANGE:

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer # importo la libreria che mi fa funzionare il server
 import time # importo la libreria che mi permette di utilizzare il tempo
-
 hostName = "localhost" # definisco il nome del mio host 
 serverPort = 8080 # definisco la porta
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == "/Lapo":
+            personalized_path = "Tapinassi"
+        else:
+            personalized_path = self.path
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % personalized_path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server daje.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
