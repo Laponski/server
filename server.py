d442c93ce30374086162146cc731beb35c4cefaa
Laponski <lapotap2007@gmail.com>
2024-06-26 16:19:40 +0200

    feat: if lapo is the path then tapinassi should appear

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ serverPort = 8080 # definisco la porta
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == "lapo":
+            self.path == "tapinassi"
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -14,7 +16,6 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server daje.</p>", "utf-8"))
-        print("%s")
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
