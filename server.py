ea7c6dd09b4260c64f96c859c18b503e2b2553ec
Laponski <lapotap2007@gmail.com>
2024-06-28 10:31:25 +0200

    feat: added a control on the value of the patch, now it must contain a number

--- server.py
+++ server.py
@@ -18,21 +18,26 @@ def get_item(key):
     if key in data:  # Controlla se la chiave esiste nel dizionario
         return jsonify({key: data[key]})  # Restituisce il valore associato alla chiave in formato JSON
     else:
-        return "Item not found", 404  # Restituisce un errore 404 se la chiave non esiste
+        return "404, Not Found", 404  # Restituisce un errore 404 se la chiave non esiste
 
 @app.route('/item', methods=['POST'])  # Definisce una route per gestire le richieste POST per creare un nuovo item
 def post_item():
     item = request.json  # Ottiene i dati JSON dalla richiesta
     key = item.get('key')  # Estrae la chiave dall'oggetto JSON
-    value = item.get('value')  # Estrae il valore dall'oggetto JSON
     x = re.search("key[1-5]", key) #la mia regex
+    value = item.get('value')  # Estrae il valore dall'oggetto JSON
+    y = re.search("[0-9]", value)
     if key in data:  # Controlla se la chiave esiste già nel dizionario
         return "Item already exists", 400  # Restituisce un errore 400 se la chiave esiste già
-    elif x: # se la regex matcha un valore allora esegue sotto
+    elif x and y: # se la regex matcha un valore allora esegue sotto
         data[key] = value  # Aggiunge il nuovo item al dizionario
         return jsonify({key: value}), 201  # Restituisce il nuovo item creato in formato JSON con un codice di stato 201
-    else: # se la regex non matcha allora restituisci sotto
-        return "There can be maximum 5 keys, try again :(", 202 # messaggio per informare l'utente e codice di uscita
+    elif not x : # se la regex non matcha allora restituisci sotto
+        return "There can be maximum 5 keys, try again.", 202 # messaggio per informare l'utente e codice di uscita
+    elif not y:
+        return "Your value must contain at least one number.", 202
+    else:
+        return "Your value must contain at least one number and there can be maximum 5 keys", 202
 
 @app.route('/item/<key>', methods=['PUT'])  # Definisce una route per gestire le richieste PUT per creare o aggiornare un item specifico
 def put_item(key):
@@ -47,7 +52,7 @@ def patch_item(key):
         data[key] = value  # Aggiorna il valore della chiave esistente nel dizionario
         return jsonify({key: value})  # Restituisce l'item aggiornato in formato JSON
     else:
-        return "Item not found", 404  # Restituisce un errore 404 se la chiave non esiste
+        return "404, Not Found", 404  # Restituisce un errore 404 se la chiave non esiste
 
 @app.route('/item/<key>', methods=['DELETE'])  # Definisce una route per gestire le richieste DELETE per eliminare un item specifico
 def delete_item(key):
@@ -55,7 +60,7 @@ def delete_item(key):
         del data[key]  # Elimina la voce associata alla chiave dal dizionario
         return '', 204  # Restituisce un codice di stato 204 senza contenuto
     else:
-        return "Item not found", 404  # Restituisce un errore 404 se la chiave non esiste
+        return "404, Not Found", 404  # Restituisce un errore 404 se la chiave non esiste
 
 if __name__ == '__main__':  # Verifica se il file viene eseguito direttamente e non importato come modulo
     app.run(debug=True)  # Avvia il server in modalità debug
