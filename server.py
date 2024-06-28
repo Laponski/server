96babd65dee5bc5af0ffa917ec268d44505de88c
Laponski <lapotap2007@gmail.com>
2024-06-28 15:10:06 +0200

    feat: changed that now there can be maximum 5 keys, not the numbere after the key
    BREAKING CHANGE:

--- server.py
+++ server.py
@@ -30,43 +30,44 @@ def get_list():
 
 @app.route('/item', methods=['POST'])  # Definisce una route per gestire le richieste POST per creare un nuovo item
 def post_item():
+    x = len(data)
     item = request.json  # Ottiene i dati JSON dalla richiesta
     key = item.get('key')  # Estrae la chiave dall'oggetto JSON
-    x = re.search("key[1-5]", key) #la mia regex
+    x += 1
     value = item.get('value')  # Estrae il valore dall'oggetto JSON
     y = re.search("[0-9]", value)
     if key in data:  # Controlla se la chiave esiste già nel dizionario
         return "Item already exists, try deleting some keys before adding new ones", 400  # Restituisce un errore 400 se la chiave esiste già
-    elif x and y: # se la regex matcha un valore allora esegue sotto
+    elif x < 6 and y: # se la regex matcha un valore allora esegue sotto
         data[key] = value  # Aggiunge il nuovo item al dizionario
         return jsonify({key: value}), 201  # Restituisce il nuovo item creato in formato JSON con un codice di stato 201
-    elif not x and not y: # se la regex non matcha allora restituisci sotto
+    elif x > 5 and not y: # se la regex non matcha allora restituisci sotto
         return "Your value must contain at least one number and there can be maximum 5 keys", 202 # messaggio per informare l'utente e codice di uscita
     elif not y:
         return "Your value must contain at least one number.", 202
-    elif not x:
+    elif x > 5:
         return "There can be maximum 5 keys, try again.", 202
 
 @app.route('/item/<key>', methods=['PUT'])  # Definisce una route per gestire le richieste PUT per creare o aggiornare un item specifico
 def put_item(key):
-    x = re.search("key[1-5]", key) #la mia regex
+    x = len(data)
+    x += 1
     value = request.json.get('value')  # Ottiene il valore dalla richiesta JSON
     y = re.search("[0-9]", value)
-    if x and y:
+    if x < 6 and y:
         data[key] = value  # Aggiorna o crea una nuova voce nel dizionario con la chiave specificata
         return jsonify({key: value}), 201  # Restituisce l'item creato o aggiornato in formato JSON con un codice di stato 201
-    elif  not x and not y:
+    elif  x > 5 and not y:
         return "Your value must contain at least one number and there can be maximum 5 keys.", 202
     elif not y:
         return "Your value must contain at least one number.", 202
-    elif not x:
+    elif x > 5:
         return "There can be maximum 5 keys, try again.", 202
-
-    
 
 @app.route('/item/<key>', methods=['PATCH'])  # Definisce una route per gestire le richieste PATCH per aggiornare parzialmente un item specifico
 def patch_item(key):
-    x = re.search("key[1-5]", key) #la mia regex
+    x = len(data)
+    x += 1
     if key in data:  # Controlla se la chiave esiste nel dizionario
         value = request.json.get('value')  # Ottiene il valore dalla richiesta JSON
         y = re.search("[0-9]", value)
@@ -75,7 +76,7 @@ def patch_item(key):
             return jsonify({key: value})  # Restituisce l'item aggiornato in formato JSON
         else:
             return "Your value must contain at least one number.", 202
-    elif not x:
+    elif x > 5:
             return "404, Not Found. Remember that there are maximum 5 keys", 404
     else:
         return "404, Not Found", 404  # Restituisce un errore 404 se la chiave non esiste
